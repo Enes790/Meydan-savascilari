095509ee7da54231193e88477683a7d8370dfdaa
Enes790 <menesakaydin@gmail.com>
2026-09-23 11:39:51 +0300

    Create js/zombies.js

---
+++ js/js/js/zombies.js
@@ -0,0 +1,75 @@
+import {Entity} from './entities.js';
+import {CFG, ZB, BADGE, PL} from './config.js';
+
+export class Zombie extends Entity {
+  constructor(type, row, x, y, w, h){
+    super(x, y, w, h);
+    this.type = type;
+    this.row = row;
+    const d = ZB[type];
+    this.hp = d.hp;
+    this.mhp = d.hp;
+    this.speed = d.spd;
+    this.dmg = d.dmg;
+    this.color = d.col;
+    this.weakenTimer = 0;
+    this.burnTimer = 0;
+  }
+
+  update(dt, g){
+    if(this.weakenTimer > 0) this.weakenTimer -= dt;
+    if(this.burnTimer > 0){
+      this.hit(CFG.BURN_D * dt);
+      this.burnTimer -= dt;
+    }
+    const dmg = this.weakenTimer > 0 ? this.dmg * PL.ignear.wm : this.dmg;
+    const target = g.plantInFront(this);
+    if(target) target.hit(dmg * dt);
+    else this.x -= this.speed * dt;
+    if(this.x + this.w < g.board.ox) g.gameOver();
+  }
+
+  hit(d){
+    this.hp -= d;
+    if(this.hp <= 0) this.alive = 0;
+  }
+
+  draw(ctx){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    if(this.weakenTimer > 0){
+      ctx.fillStyle = "rgba(160,80,220,.35)";
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+    if(this.burnTimer > 0){
+      const pu = .4 + .3 * Math.sin(performance.now()/100);
+      ctx.fillStyle = `rgba(255,120,0,${pu})`;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+      ctx.font = `${this.h*.35}px serif`;
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText("🔥", this.x+this.w*.5, this.y+this.h*.15);
+    }
+    ctx.font = `${this.h*.55}px serif`;
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
+    ctx.fillText("🧟", this.x, this.y+this.h/2);
+    const badge = BADGE[this.type];
+    if(badge){
+      ctx.font = `${this.h*(this.type==="armored"?.35:.3)}px serif`;
+      ctx.textAlign = this.type==="runner" ? "left" : "center";
+      ctx.fillText(badge,
+        this.type==="runner" ? this.x+this.w*.55 : this.x+this.w*.5,
+        this.y + this.h*(this.type==="runner"?.25:.3));
+    }
+    if(this.weakenTimer > 0){
+      ctx.font = `${this.h*.28}px serif`;
+      ctx.textAlign = "center";
+      ctx.fillText("💜", this.x+this.w*.85, this.y+this.h*.15);
+    }
+    ctx.fillStyle = "#000";
+    ctx.fillRect(this.x, this.y-4, this.w, 3);
+    ctx.fillStyle = this.burnTimer>0 ? "#ff8c00" : "#f00";
+    ctx.fillRect(this.x, this.y-4, this.w*(this.hp/this.mhp), 3);
+  }
+}
