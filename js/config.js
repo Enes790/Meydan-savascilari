fb162da6f9eed66357825200b1fb127862ce557a
Enes790 <menesakaydin@gmail.com>
2026-09-23 14:50:35 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -18,7 +18,7 @@ export const PL = {
   peashooter:{n:"Bezelye",c:100,hp:300,col:"#4caf50",em:"🌱",dmg:25,cd:1.2},
   wallnut:{n:"Ceviz",c:50,hp:4000,col:"#8b5a2b",em:"🌰"},
   mine:{n:"Mayın",c:25,hp:300,col:"#c0392b",em:"💣",arm:15,dmg:400,max:3},
-  ignear:{n:"İğneatar",c:125,hp:300,col:"#8e44ad",em:"🏹",dmg:15,cd:1.8,pierce:3,rt:4.5,wt:1.5,wm:.6},
+  ignear:{n:"İğneatar",c:125,hp:300,col:"#8e44ad",em:"🏹",dmg:20,cd:1.8,pierce:3,rt:4.5,wt:1.5,wm:.6},
   spike:{n:"Diken",c:75,hp:300,col:"#6b4226",em:"🪵",dmg:15,tick:1,max:4},
   anka:{n:"Anka",c:75,hp:700,col:"#e67e22",em:"🌺",hitD:15,hitCD:1.4,rt:1,gT:8,gD:65,gRT:1.5},
   shifaci:{n:"Şifacı",c:75,hp:300,col:"#2ecc71",em:"⚕️",heal:600,shield:150,cd:20,rt:1.5},
@@ -26,7 +26,7 @@ export const PL = {
   alev:{n:"Alev Çiçeği",c:100,hp:300,col:"#e74c3c",em:"🔥",fire:5,direct:15,bT:6,max:3,lRT:1.5},
   ruzgar:{n:"Rüzgar Topu",c:25,hp:300,col:"#90caf9",em:"🌪️",shots:6,interval:0.5,push:0.25,stun:0.3},
  buzul:{n:"Buzul Çiçeği",c:125,hp:500,col:"#4fc3f7",em:"🧊",slow:0.3,auraR:1.5,freezeT:3},
-zipkin:{n:"Zıpkın",c:125,hp:300,col:"#8d6e63",em:"🎣",dmg:20,cd:2.1,pierce:5,rt:4.5},
+zipkin:{n:"Zıpkın",c:125,hp:300,col:"#8d6e63",em:"🎣",dmg:20,cd:1.8,pierce:5,rt:4.5},
 tepkiliMayin:{n:"Tepkili Mayın",c:150,hp:300,col:"#7b1fa2",em:"💣",dmg:400,max:3,arm:15,reload:25},
 cehennem:{n:"Cehennem Çiçeği",c:175,hp:300,col:"#c0392b",em:"🌋",rt:4.5}
 };
